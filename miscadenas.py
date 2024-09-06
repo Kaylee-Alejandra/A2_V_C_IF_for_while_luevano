d64afde9e86df984b3c4ce890f4c5fc3b76eb19f
kaylee luevano molina <a.22308051281248@cbtis128.edu.mx>
2024-09-06 09:46:32 -0600

    Add files via upload
    agrege dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,22 @@
+'''Kaylee Luevano '''
+
+nombre= "kaylee"
+print(nombre[0])
+print(nombre)
+
+Misnovios=["K", "A", "Y"]
+for chicos in Misnovios:
+ print(chicos)
+
+
+print ("otro ejemplo")
+b = "KayleeLuevano"
+print(b[2:5])
+print("a mayusculas")
+a = "keili buebano"
+print(a.upper())
+print("Concatenar")
+a = "Maria"
+b = "Perez"
+c = a + " " + b
+print(c)
