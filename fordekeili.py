5738b962d9eb33f4ed0c47537f3894640cfb6cd0
kaylee luevano molina <a.22308051281248@cbtis128.edu.mx>
2024-09-06 10:16:53 -0600

    Add files via upload
    while y for

---
+++ fordekeili.py
@@ -0,0 +1,5 @@
+mascotas = ["pato", "perro", "gato"] ## se almacenan los datos 
+for x in mascotas:
+  if x == "perro": ## se excluye este dato 
+    continue
+  print(x) ## muestra 
---
+++ miwhile.py
@@ -0,0 +1,6 @@
+i = 1 ## se le asigna valor
+while i < 6: # el valor aumenta en menor cantidad a 6
+  print(i)
+  i += 1
+else:
+  print("la secuencia llega hasta 5")
